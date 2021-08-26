3f44ec04f13b6a666b4acdd440e9375b86bc009d
hirasatatsuya <omametan1229@gmail.com>
2021-08-26 15:59:04 +0900

    add welcome

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('create', 'App\Http\Controllers\CreateController@index');
 
